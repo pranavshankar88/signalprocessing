ea34127f2385812fc4c2ce1ddee0f90b22c9079d
pranavshankar88 <pranav.shankar@research.iiit.ac.in>
2024-08-09 10:28:14 +0530

    LAb 0

---
+++ half_sin_signal.m
@@ -0,0 +1,23 @@
+% PLlot 5 sine waves on a single plot
+t = 0 : 1/1000 : 1;
+% f=[1];
+f=1;
+% % T=[t;2*t;3*t;4*t;5*t];
+% 
+% figure();
+sig1=sin(pi*f'*t);
+% % stem(t,sig1);
+% plot(t,sig1);
+% grid("on");
+
+% Plot a square wave of 2 wave forms with duty cycle of 30% 
+% x=[ones(1,3),zeros(1,7),ones(1,3),zeros(1,7)];
+% stem(x,);
+
+%chutiyapa
+% ein
+% x=0:1/1000:5;
+% sig=sin(x.^2);
+% plot(x,sig);
+
+% zwei
